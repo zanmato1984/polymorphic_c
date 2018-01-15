d688d29b31fc26de4bcbb953952275b8d25bc218
zanmato <ruoxi.sun@transwarp.io>
2018-01-15 20:00:18 +0800

    Save work for pc.

---
+++ pc/main.c
@@ -0,0 +1,67 @@
+#include "polymorphic_c.h"
+
+#include <stdio.h>
+
+BEGIN_DEF_CLASS(base, object)
+END_DEF_CLASS(base, object)
+
+DECL_CLASS_VFUNCS(base, object,
+    get_name,
+    say_hello)
+
+DEF_VFUNC(base, const char *, get_name) {
+  return "base";
+}
+
+DEF_VFUNC(base, void, say_hello) {
+  printf("Hello, I'm %s.\n", VFUNC_CALL(this, get_name));
+}
+
+REGISTER_CLASS_VFUNCS(base, object,
+    get_name, VFUNC_REF(base, get_name),
+    say_hello, VFUNC_REF(base, say_hello))
+
+BEGIN_DEF_CLASS(derived, base)
+  char *name;
+END_DEF_CLASS(derived, base)
+
+DECL_CLASS_VFUNCS(derived, base,
+    set_name)
+
+OVERRIDE_VFUNC(derived, const char *, get_name) {
+  if (this->name == NULL) {
+    return "derived";
+  }
+  return this->name;
+}
+
+DEF_VFUNC(derived, void, set_name, char *name) {
+  this->name = name;
+}
+
+REGISTER_CLASS_VFUNCS(derived, base,
+    get_name, VFUNC_REF(derived, get_name),
+    set_name, VFUNC_REF(derived, set_name))
+
+REGISTER_CLASSES(
+    CLASS_REF(base),
+    CLASS_REF(derived))
+
+int main() {
+  polymorphic_c_init();
+
+  base *b = new_base();
+  derived *d = new_derived();
+  base *bd = d;
+
+  VFUNC_CALL(b, say_hello);
+  VFUNC_CALL(bd, say_hello);
+  VFUNC_CALL(d, set_name, "renamed");
+  VFUNC_CALL(bd, say_hello);
+  VFUNC_CALL(d, say_hello);
+
+  delete(b);
+  delete(bd);
+
+  return 0;
+}
---
+++ pc/polymorphic_c.h
@@ -0,0 +1,115 @@
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_VFUNC 256
+
+// virtual function type
+typedef void(*__vfunc_t)();
+
+// single inheritance root class object
+__vfunc_t __object_vtable[1];
+
+typedef struct {
+  __vfunc_t *__vptr;
+} object;
+
+void __object_ctor(object *this);
+
+typedef void (*__dtor_t)(object *this);
+
+void __object_dtor(object *this);
+
+enum __object_vfuncs {
+  dtor = 0,
+  __object_last_vfunc,
+};
+
+void __register_object_vfuncs(__vfunc_t vtable[]);
+
+// vtable manipulation functions
+void __register_vfuncs(__vfunc_t vtable[], ...);
+
+// universal object delete function
+void delete(object *p);
+
+// macros to define classes
+#define BEGIN_DEF_CLASS(c, p) \
+  __vfunc_t __##c##_vtable[MAX_VFUNC]; \
+  typedef struct { \
+    __vfunc_t *__vptr; \
+    p __parent;
+
+#define END_DEF_CLASS(c, p) \
+  } c; \
+  void __##c##_ctor(c *this) { \
+    __##p##_ctor(&this->__parent); \
+    this->__vptr = __##c##_vtable; \
+  } \
+  c *new_##c() { \
+    c *this = malloc(sizeof(c)); \
+    memset(this, 0, sizeof(c)); \
+    __##c##_ctor(this); \
+    return this; \
+  } \
+  void __##c##_dtor(c *this) { \
+    __##p##_dtor(this); \
+  }
+
+#define __VFUNC_NAME(c, f) \
+  __##c##_##f
+
+#define __VFUNC_TYPE_NAME(f) \
+  __##f##_t
+
+// macro to reference a vfunc when registering
+#define VFUNC_REF(c, f) \
+  __VFUNC_NAME(c, f)
+
+// macros to define virtual functions
+#define OVERRIDE_VFUNC(c, r, f, ...) \
+  r __VFUNC_NAME(c, f)(c *this, ##__VA_ARGS__)
+
+#define DEF_VFUNC(c, r, f, ...) \
+  typedef r (*__VFUNC_TYPE_NAME(f))(c *this, ##__VA_ARGS__); \
+  OVERRIDE_VFUNC(c, r, f, ##__VA_ARGS__)
+
+// macro to declare subclass-only virtual functions
+#define DECL_CLASS_VFUNCS(c, p, ...) \
+  enum __##c##_vfuncs { \
+    __##c##_first_vfunc = __##p##_last_vfunc, \
+    __VA_ARGS__, \
+    __##c##_last_vfunc, \
+  }; \
+
+#define __REGISTER_CLASS_FUNC_NAME(c) \
+  __register_##c
+
+// macro to reference a class when registering
+#define CLASS_REF(c) \
+  __REGISTER_CLASS_FUNC_NAME(c)
+
+// macro to register subclass-implemented (new or overriden) virtual functions
+#define REGISTER_CLASS_VFUNCS(c, p, ...) \
+  void __register_##c##_vfuncs(__vfunc_t vtable[]) { \
+    __register_##p##_vfuncs(vtable); \
+    __register_vfuncs(vtable, __VA_ARGS__, dtor, __##c##_dtor, MAX_VFUNC); \
+  } \
+  void __REGISTER_CLASS_FUNC_NAME(c)() { \
+    __register_##c##_vfuncs(__##c##_vtable); \
+  }
+
+// class virtual function registering macros
+typedef void(*__register_class_func_t)();
+
+void __register_classes(int dummy, ...);
+
+#define REGISTER_CLASSES(...) \
+  void polymorphic_c_init() { \
+    __register_classes(0, ##__VA_ARGS__, NULL); \
+  }
+
+#define VFUNC_CALL(this, f, ...) \
+  ((__VFUNC_TYPE_NAME(f))(this->__vptr[f]))(this, ##__VA_ARGS__)
+
+#define PARENT_VFUNC_CALL(this, f, ...) \
+  VFUNC_CALL(this->__parent, f, ##__VA_ARGS__)
